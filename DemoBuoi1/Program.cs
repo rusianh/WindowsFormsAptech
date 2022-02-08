e00a9aacb3c1e1e1fe2ef9a203a7e868005a184b
VietAnhVu <vva1994@gmail.com>
2022-02-08 16:38:38 +0700

    Add project files.

---
+++ DemoBuoi1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoBuoi1
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1()); // file chính của chương trình ( đảm bảo nó luôn luôn chạy, ko bao giờ đc đóng)
+        }
+    }
+}
