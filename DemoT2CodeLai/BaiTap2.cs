45185d55b29f3aed9ac1d9617446cbad6286a8ba
VietAnhVu <vva1994@gmail.com>
2022-02-10 19:08:49 +0700

    DemoT2Bai1

---
+++ DemoT2CodeLai/BaiTap2.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DemoT2CodeLai
+{
+    public partial class BaiTap2 : Form
+    {
+        public BaiTap2()
+        {
+            InitializeComponent();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnChange_Click(object sender, EventArgs e)
+        {
+            if (lbWord.Visible) {
+                lbWord.Visible = false;
+                lbMean.Visible = true;
+            }
+            else { lbWord.Visible = true; lbMean.Visible = false; }
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtWord.Text) && !string.IsNullOrEmpty(txtMean.Text)) {
+                lbWord.Items.Add(txtWord.Text);
+                lbMean.Items.Add(txtMean.Text);
+            }
+        }
+
+        private void lbWord_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbWord.SelectedIndex != -1) {
+                int index = lbWord.SelectedIndex;
+                string word = lbMean.Items[index].ToString();
+                txtDataMean.Text = word;
+            }
+        }
+
+        private void lbMean_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (lbMean.SelectedIndex != -1) {
+                int index = lbMean.SelectedIndex;
+                string word = lbWord.Items[index].ToString();
+                txtDataMean.Text = word;
+            }
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(txtSearch.Text)) return;
+            ListBox lb;
+            if (lbMean.Visible) lb = lbMean;
+            else lb = lbWord; // lb tro den vi tri cua lb word
+            for (int i = 0; i < lb.Items.Count; i++) {
+                if (lb.Items[i].ToString().ToLower().Contains(txtSearch.Text.ToLower())) {
+                    lb.SelectedIndex = i;
+                    break;
+                }
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            ListBox lb;
+            if (lbMean.Visible) lb = lbMean;
+            else lb = lbWord;
+            if (lb.SelectedIndex >= 0 && MessageBox.Show("Ban co muon xoa khong", "Confirm", MessageBoxButtons.YesNoCancel) == DialogResult.Yes) {
+                int viTriXoa = lb.SelectedIndex;
+                lbWord.Items.RemoveAt(viTriXoa);
+                lbMean.Items.RemoveAt(viTriXoa);
+                MessageBox.Show("Xoa thanh cong!");
+
+            }
+        }
+    }
+}
---
+++ DemoT2CodeLai/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DemoT2CodeLai
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.baiTap2ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.thoatToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.baiTap2ToolStripMenuItem,
+            this.thoatToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // baiTap2ToolStripMenuItem
+            // 
+            this.baiTap2ToolStripMenuItem.Name = "baiTap2ToolStripMenuItem";
+            this.baiTap2ToolStripMenuItem.Size = new System.Drawing.Size(65, 20);
+            this.baiTap2ToolStripMenuItem.Text = "Bai Tap 2";
+            this.baiTap2ToolStripMenuItem.Click += new System.EventHandler(this.baiTap2ToolStripMenuItem_Click);
+            // 
+            // thoatToolStripMenuItem
+            // 
+            this.thoatToolStripMenuItem.Name = "thoatToolStripMenuItem";
+            this.thoatToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.thoatToolStripMenuItem.Text = "&Thoat";
+            this.thoatToolStripMenuItem.Click += new System.EventHandler(this.thoatToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem baiTap2ToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem thoatToolStripMenuItem;
+    }
+}
+
