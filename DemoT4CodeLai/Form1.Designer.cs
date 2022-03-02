8e193316f3ace11265916560488a0ae64239efdb
Cris Dev <vva1994@gmail.com>
2022-03-02 18:19:19 +0700

    :)

--- DemoT4CodeLai/Form1.Designer.cs
+++ DemoT4CodeLai/Form1.Designer.cs
@@ -27,13 +27,82 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtArea = new System.Windows.Forms.TextBox();
+            this.txtWeather = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnViewWeather = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnViewWeather);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.txtWeather);
+            this.groupBox1.Controls.Add(this.txtArea);
+            this.groupBox1.Location = new System.Drawing.Point(12, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(277, 425);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Weather";
+            // 
+            // txtArea
+            // 
+            this.txtArea.Location = new System.Drawing.Point(72, 35);
+            this.txtArea.Name = "txtArea";
+            this.txtArea.Size = new System.Drawing.Size(145, 20);
+            this.txtArea.TabIndex = 0;
+            // 
+            // txtWeather
+            // 
+            this.txtWeather.Location = new System.Drawing.Point(18, 78);
+            this.txtWeather.Multiline = true;
+            this.txtWeather.Name = "txtWeather";
+            this.txtWeather.Size = new System.Drawing.Size(229, 341);
+            this.txtWeather.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Khu Vuc";
+            // 
+            // btnViewWeather
+            // 
+            this.btnViewWeather.Location = new System.Drawing.Point(223, 32);
+            this.btnViewWeather.Name = "btnViewWeather";
+            this.btnViewWeather.Size = new System.Drawing.Size(48, 23);
+            this.btnViewWeather.TabIndex = 2;
+            this.btnViewWeather.Text = "Xem";
+            this.btnViewWeather.UseVisualStyleBackColor = true;
+            this.btnViewWeather.Click += new System.EventHandler(this.btnViewWeather_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtArea;
+        private System.Windows.Forms.Button btnViewWeather;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtWeather;
     }
 }
 
--- DemoT4CodeLai/Form1.cs
+++ DemoT4CodeLai/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,6 +16,18 @@ namespace DemoT4CodeLai
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnViewWeather_Click(object sender, EventArgs e)
+        {
+            string url = string.Format("http://api.openweathermap.org/data/2.5/weather?q={0}&appid=8e1908235b6bf2bdd4bfdb4af649a714", txtArea.Text);
+            HttpClient client = new HttpClient();
+            txtWeather.Text = client.GetStringAsync(url).Result;
+            // Lam 2 viec 
+            // Cai JSon.net: newtonsoft.Json
+            // Chuyen file Json quan ly bang class object
+            ClsWeather w =  Newtonsoft.Json.JsonConvert.DeserializeObject<ClsWeather>(txtWeather.Text);
+            txtWeather.Text = String.Format("Name: {0} ({1}) \r\nWeather: {2} - {3}", w.name, w.sys.country,w.weather[0].main, w.weather[0].description);
         }
     }
 }
